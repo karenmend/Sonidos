8aa14fff3a608753ce6c3556051a6710dfdac60a
Alumno <ulsa@iMacD109.local>
2019-08-21 14:46:07 -0700

    segundo intento

--- Sonido/ViewController.swift
+++ Sonido/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player : AVAudioPlayer?
+    
+    @IBAction func doTapReproducir(_ sender: Any) {
+        if player != nil {
+            player?.play()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Bundle representa al grupo o paquete al que pertenece(hay varios bundles (main=principal)
+        
+        let path = Bundle.main.path(forResource: "Dream More Than Love.mp3", ofType: nil)
+        //Transformas el path a url
+        let url = URL(fileURLWithPath: path!)
+        
+        do {
+             player = try AVAudioPlayer(contentsOf: url)
+        } catch {
+            
+        }
+       
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
